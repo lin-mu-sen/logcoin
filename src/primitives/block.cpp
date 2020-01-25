914cff68a8d9d66fbcc195788cbeec3c74aec48b
林木森 <you@example.com>
2020-01-26 04:04:05 +0800

    pubkey fixes

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -6,11 +6,13 @@
 #include <primitives/block.h>
 
 #include <hash.h>
+#include <primitives/hashblock.h>
 #include <tinyformat.h>
 #include <utilstrencodings.h>
 #include <crypto/common.h>
 #include <crypto/scrypt.h>
 
+GLOBAL sph_skein512_context     z_skein;
 uint256 CBlockHeader::GetHash() const
 {
     return SerializeHash(*this);
@@ -18,9 +20,19 @@ uint256 CBlockHeader::GetHash() const
 
 uint256 CBlockHeader::GetPoWHash() const
 {
-    uint256 thash;
-    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
-    return thash;
+        //printf("Our block: %s \n", ToString().c_str());
+        //printf("Inside block header get PoWHash \n");
+        //printf(" hi there %s \n", this->ToString().c_str());
+        //printf("Performing pow hash on block of time %i \n", nTime);
+        uint256 thash, thash2;
+        //scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+        skein2hash(&thash2, BEGIN(nVersion));
+
+        //thash = Hash2(BEGIN(nVersion), END(nNonce));
+        //thash = Hash2(BEGIN(nVersion), END(nNonce));
+        //printf("Thash Out Is: %s \n ",thash.ToString().c_str());
+        //printf("Thash2 Out Is: %s \n ",thash2.ToString().c_str());
+        return thash2;
 }
 
 std::string CBlock::ToString() const
